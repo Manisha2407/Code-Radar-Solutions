449a5d535bd293fadc10519f6be614c416e6bd0e
Manisha2407 <manishaduggal06@gmail.com>
2025-02-07 11:21:26 +0530

    Solution to Logical AND Operation

---
+++ Logical AND Operation@@67882a731b4392d8fc1577dc/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    //&& this is and oprator it will return true if both the statements are true otherwise return false
+    int a,b;
+    scanf("%d%d",&a ,&b);
+    if(a>0 && b>0){
+        printf("True");
+    }
+    if(a<0 && b<0){
+    printf("False");
+    }
+    return 0;
+}
