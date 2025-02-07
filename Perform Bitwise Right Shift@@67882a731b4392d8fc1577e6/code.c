6d2d7c31b227aa4ee87d271af284b583f4d52269
Manisha2407 <manishaduggal06@gmail.com>
2025-02-07 16:08:24 +0530

    Solution to Perform Bitwise Right Shift

--- Perform Bitwise Right Shift@@67882a731b4392d8fc1577e6/code.c
+++ Perform Bitwise Right Shift@@67882a731b4392d8fc1577e6/code.c
@@ -5,7 +5,7 @@ int main()
 
     int num,shift,result;
     scanf("%d%d",&num ,&shift);
-    result=num>>shift;
+    result=num>>shift;//eg num is 32 and shift is 2 so 32/2*2(4)is 32/4 is equal to 8
     printf("%d",result);
     return 0;
 }
