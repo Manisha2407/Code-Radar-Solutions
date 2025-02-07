155268f38f19071d5f665b4c40361101036b85e9
Manisha2407 <manishaduggal06@gmail.com>
2025-02-07 12:52:48 +0530

    Solution to Perform Bitwise OR

---
+++ Perform Bitwise OR@@67882a731b4392d8fc1577e2/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int a,b,c;
+    // bitwise or operator convert firstly the number into binary for eg=or a = 5 and b = 3:
+
+//Binary of 5 → 0101
+//Binary of 3 → 0011
+//Bitwise OR (|) result → 0111 (which is 7 in decimal).
+scanf("%d%d",&a ,&b);
+c=a|b;
+printf(c);
+return 0;
+}
